daf455602000f7f3df09293ee03e3bff182e0d45
Samura801 <angel.landa.cab@gmail.com>
2025-10-22 23:23:01 -0600

    MejoraV2

---
+++ Android/app/src/main/java/com/example/solinx/NotificacionesEmp.java
@@ -0,0 +1,53 @@
+package com.example.solinx;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class NotificacionesEmp extends AppCompatActivity implements View.OnClickListener {
+    Button menu, notificaciones, ad1, ad2, ad3, ne1, ne2, ne3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_notificaciones_emp);
+
+        menu = findViewById(R.id.btnMenu);
+        menu.setOnClickListener(this);
+        notificaciones = findViewById(R.id.btnNotificaciones);
+        notificaciones.setOnClickListener(this);
+        ad1 = findViewById(R.id.admitir);
+        ad1.setOnClickListener(this);
+        ad2 = findViewById(R.id.admitir2);
+        ad2.setOnClickListener(this);
+        ad3 = findViewById(R.id.admitir3);
+        ad3.setOnClickListener(this);
+        ne1 = findViewById(R.id.rechazar);
+        ne1.setOnClickListener(this);
+        ne2 = findViewById(R.id.rechazar2);
+        ne2.setOnClickListener(this);
+        ne3 = findViewById(R.id.rechazar3);
+        ne3.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String cadena = ((Button)v).getText().toString();
+        if (cadena.equals("Menú")) {
+            Toast.makeText(this, "Pantalla Menú...", Toast.LENGTH_SHORT).show();
+        } if (cadena.equals("Notificaciones")) {
+            Toast.makeText(this, "Estas en Notificaciones...", Toast.LENGTH_SHORT).show();
+        } if (cadena.equals("Admitir")) {
+            Toast.makeText(this, "Usuario Admitido...", Toast.LENGTH_SHORT).show();
+        } if (cadena.equals("Rechazar")) {
+            Toast.makeText(this, "Usuario Rechazado...", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
