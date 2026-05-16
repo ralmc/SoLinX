5aa5c7ada2b3256bf6231d1eb95a7948ee4c1974
Samera801 <angel.landa.cab@gmail.com>
2026-05-15 19:26:34 -0600

    Alumno Notificaciones WEB

--- HTML/SoLinX-WEB/web/js/api.js
+++ HTML/SoLinX-WEB/web/js/api.js
@@ -3,7 +3,7 @@
  * Cambia BASE_URL por la IP de tu computadora
  */
  
-const BASE_URL = 'http://192.168.1.97:8080/SoLinX/api';
+const BASE_URL = 'http://192.168.1.69:8080/SoLinX/api';
  
 async function request(method, endpoint, body = null, isMultipart = false) {
   const headers = {};
